0421eaef2a031e8c2162a31eeaa3f952ae23310f
tomasz.michalak <tomek.michalak@gmail.com>
2018-04-04 09:12:40 +0200

    Adding bridge knot.

---
+++ knot/src/main/java/io/knotx/databridge/knot/impl/FragmentProcessor.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2018 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.knotx.databridge.knot.impl;
+
+import java.util.concurrent.ExecutionException;
+
+import io.knotx.databridge.knot.ServiceKnotOptions;
+import io.knotx.databridge.knot.service.ServiceEngine;
+import io.knotx.databridge.knot.service.ServiceEntry;
+import io.knotx.dataobjects.KnotContext;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.core.Vertx;
+
+public class FragmentProcessor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(FragmentProcessor.class);
+
+  private final ServiceEngine serviceEngine;
+
+  public FragmentProcessor(Vertx vertx, ServiceKnotOptions options) {
+    this.serviceEngine = new ServiceEngine(vertx, options);
+  }
+
+  public Single<FragmentContext> processSnippet(final FragmentContext fragmentContext,
+      KnotContext request) {
+    if (LOGGER.isTraceEnabled()) {
+      LOGGER.trace("Processing Handlebars snippet {}", fragmentContext.fragment());
+    }
+    return Observable.just(fragmentContext)
+        .flatMap(FragmentContext::services)
+        .map(serviceEngine::mergeWithConfiguration)
+        .doOnNext(this::traceService)
+        .flatMap(serviceEntry ->
+            fetchServiceData(serviceEntry, request).toObservable()
+                .map(serviceEntry::getResultWithNamespaceAsKey))
+        .reduce(new JsonObject(), JsonObject::mergeIn)
+        .map(results -> applyData(fragmentContext, results));
+  }
+
+  private Single<JsonObject> fetchServiceData(ServiceEntry service, KnotContext request) {
+    LOGGER.debug("Fetching data from service {} {}", service.getAddress(), service.getParams());
+    try {
+      return request.getCache()
+          .get(service.getCacheKey(), () -> {
+            LOGGER.debug("Requesting data from adapter {} with params {}", service.getAddress(),
+                service.getParams());
+            return serviceEngine.doServiceCall(service, request).cache();
+          });
+    } catch (ExecutionException e) {
+      LOGGER.fatal("Unable to get service data {}", e);
+      return Single.error(e);
+    }
+  }
+
+  private FragmentContext applyData(final FragmentContext fragmentContext,
+      JsonObject serviceResult) {
+    LOGGER.trace("Applying data to snippet {}", fragmentContext);
+    fragmentContext.fragment().context().mergeIn(serviceResult);
+    return fragmentContext;
+  }
+
+  private void traceService(ServiceEntry serviceEntry) {
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Found service call definition: {} {}", serviceEntry.getAddress(),
+          serviceEntry.getParams());
+    }
+  }
+
+}
---
+++ knot/src/test/java/io/knotx/databridge/knot/service/ServiceEntryTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2018 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.knotx.databridge.knot.service;
+
+
+import org.jsoup.nodes.Attribute;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.knotx.databridge.knot.ServiceKnotOptions;
+import io.knotx.junit.util.FileReader;
+import io.vertx.core.json.JsonObject;
+
+public class ServiceEntryTest {
+
+  private ServiceKnotOptions configWithDefaultParams;
+
+  private ServiceKnotOptions configNoDefaultParams;
+
+  @Before
+  public void setUp() throws Exception {
+    configWithDefaultParams = new ServiceKnotOptions(
+        new JsonObject(FileReader.readText("service-correct.json"))
+    );
+    configNoDefaultParams = new ServiceKnotOptions(
+        new JsonObject(FileReader.readText("service-correct-no-params.json"))
+    );
+  }
+
+
+  @Test
+  public void mergePayload_pathFromParamsAttribute() {
+    ServiceEntry serviceEntry = new ServiceEntry(
+        new Attribute("data-knotx-service-first", "first-service"),
+        new Attribute("data-knotx-params-first", "{\"path\":\"first-service\"}"));
+    serviceEntry.mergeParams(configWithDefaultParams.getServices().stream().findFirst().get().getParams());
+    Assert.assertEquals("first-service", serviceEntry.getParams().getString("path"));
+  }
+
+  @Test
+  public void mergePayload_pathFromConfigAttribute() {
+    ServiceEntry serviceEntry = new ServiceEntry(
+        new Attribute("data-knotx-service-first", "first-service"),
+        new Attribute("data-knotx-params-first", "{}"));
+    serviceEntry.mergeParams(configWithDefaultParams.getServices().stream().findFirst().get().getParams());
+    Assert.assertEquals("/service/mock/first.json", serviceEntry.getParams().getString("path"));
+  }
+
+  @Test
+  public void mergePayload_nameFromParamsAttribute() {
+    ServiceEntry serviceEntry = new ServiceEntry(
+        new Attribute("data-knotx-service-first", "first-service"),
+        new Attribute("data-knotx-params-first", "{\"name\":\"first-service-name\"}"));
+    serviceEntry.mergeParams(configWithDefaultParams.getServices().stream().findFirst().get().getParams());
+    Assert.assertEquals("/service/mock/first.json", serviceEntry.getParams().getString("path"));
+    Assert.assertEquals("first-service-name", serviceEntry.getParams().getString("name"));
+  }
+
+  @Test
+  public void mergePayload_whenNoDefaultParams_expectDefinedParamsUsed() throws Exception {
+    ServiceEntry serviceEntry = new ServiceEntry(
+        new Attribute("data-knotx-service-first", "first-service"),
+        new Attribute("data-knotx-params-first", "{\"path\":\"some-other-service.json\"}"));
+    serviceEntry.mergeParams(configNoDefaultParams.getServices().stream().findFirst().get().getParams());
+    Assert.assertEquals("some-other-service.json", serviceEntry.getParams().getString("path"));
+  }
+}
